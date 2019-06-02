ff468b69419318697f424b9d111138c6e9224329
gmdmgithub <gmika@interia.pl>
2019-06-02 18:04:17 +0200

    final amend'

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -2,8 +2,9 @@ const cacheName = 'v1';
 
 //first way - specific what you want
 const cacheAssets = [
-    'index.html','about.html',
-    '/css/style.css','/js/main.js'
+    'index.html', 'about.html',
+    '/css/style.css', '/js/main.js',
+    '/css/loader-h.css','/css/loader-a.css'
 ];
 
 //call install event
@@ -12,7 +13,7 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(cacheName)
         .then(cache => {
-            cache.addAll(cacheAssets);//main function - addAll
+            cache.addAll(cacheAssets); //main function - addAll
             console.log('cacheting files', cacheAssets);
         })
         .then(() => {
@@ -30,7 +31,7 @@ self.addEventListener('activate', (event) => {
     console.log('service Worker activated - remove old caches');
     event.waitUntil(
         caches.keys()
-        .then(cacheNames => {  ///we remove all other than current cache
+        .then(cacheNames => { ///we remove all other than current cache
             return Promise.all(
                 cacheNames.map(cache => { //possible better use of filter
                     if (cache !== cacheName) {
@@ -43,27 +44,26 @@ self.addEventListener('activate', (event) => {
     )
 });
 
-self.addEventListener('fetch', differentApproach);
+// self.addEventListener('fetch', differentApproach);
+self.addEventListener('fetch', standardApproach);
 
-// //call fetch event -offline
-// self.addEventListener('fetch', event => {
-//     console.log('service worker is fetching');
-//     //if we are offline cache and respond form cache
-//     event.respondWith(
-//         fetch(event.request) //in this place we just fetch the request - in case of problem (catch on promise) take from cache
-//         .catch(() => 
-//             caches.match(event.request)));/// in case of error load
-// });
-
+function standardApproach(event) {
+    // call fetch event -offline
+    console.log('service worker is fetching - standard approach');
+    //if we are offline cache and respond form cache
+    event.respondWith(
+        fetch(event.request)
+        .catch(() => caches.match(event.request))); 
+}
 
 //call fetch event -always - if not in cache take from request!!
-function differentApproach(event){
+function differentApproach(event) {
     console.log("service worker fetching - approach with first checking cache (version is important!!)")
 
     //check cache and respond form cache
     event.respondWith(
         caches.match(event.request)
-        .then(cacheRequest =>{
+        .then(cacheRequest => {
             console.log('response is here');
             return cacheRequest || fetch(event.request);
         })
